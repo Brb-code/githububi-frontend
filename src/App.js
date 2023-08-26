37ee9e30cf836a116f7c1f3fc93dc923c78af4f3
Israel TPC <israelhuallpara@gmail.com>
2023-08-26 12:06:53 -0500

    inicio del proyecto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input type='text' placeholder='Usuario' />
+      </header>
+    </div>
+  );
+}
+
+export default App;
